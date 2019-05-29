7f62dc89dbde4fb448b90f4e47c4ae61a5c47b49
Roberto <rr92va@gmail.com>
2019-05-29 20:07:04 +0200

    Modificado metodo configuraLineasDePedido

--- PracticaDIS/src/es/uva/eii/ds/empresaX/negocio/modelos/PedidoDeHorno.java
+++ PracticaDIS/src/es/uva/eii/ds/empresaX/negocio/modelos/PedidoDeHorno.java
@@ -62,12 +62,19 @@ public class PedidoDeHorno {
         JsonArray jLineas = jo.getAsJsonArray(JSONHelper.JSON_LINEAS);
         for(JsonElement jlin : jLineas) {
             JsonObject jLinea = new Gson().fromJson(jlin.toString(), JsonObject.class);
-          //  Producto producto = new Producto(jLinea.get(JsonHelper.JSON_PRODUCTO).toString());
-
             int cantidad =  jLinea.get(JSONHelper.JSON_CANTIDAD).getAsInt();
-          //  LineaDePedidoDeHorno lineaPedidoHorno = new LineaDePedidoDeHorno(cantidad, producto);
-           // lineas.add(lineaPedidoHorno);
-            //lineas.add(new LineaDePedidoDeHorno(cantidad, productoPedido));
+
+            //Creando Producto a través de Json
+            JsonObject pro = new Gson().fromJson(jLinea.get(JSONHelper.JSON_PRODUCTO).toString(), JsonObject.class);
+            String codigo  = pro.get(JSONHelper.JSON_CODIGO).getAsString();
+            String nombre  = pro.get(JSONHelper.JSON_NOMBRE).getAsString();
+            String descripcion = pro.get(JSONHelper.JSON_DESCRIPCION).getAsString();
+            int existencias = pro.get(JSONHelper.JSON_EXISTENCIAS).getAsInt();
+            int cantidadMinimaEnStock = pro.get(JSONHelper.JSON_CANTIDAD_MIN_STOCK).getAsInt();
+            Producto producto = new Producto(codigo, nombre, descripcion, existencias, cantidadMinimaEnStock) {};
+            
+            LineaDePedidoDeHorno lineaPedidoHorno = new LineaDePedidoDeHorno(cantidad, producto);
+            lineas.add(lineaPedidoHorno);
         }
         
     }
