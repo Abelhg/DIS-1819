8ccdc069a717d56244b229f6fc21d60578f44379
MrPalillin <daniel.vicente@alumnos.uva.es>
2019-05-29 22:35:38 +0200

    Reorganizando carpetas

--- PracticaDIS/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-import es.uva.eii.ds.empresaX.interfaz.GestorUI;
-
-
-public class Main {
-    public static void main(String[] args){
-        GestorUI gestorIU = GestorUI.getInstanciaSingleton();
-    }
-}
